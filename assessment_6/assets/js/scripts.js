5b5300ee8a8c71e0e967f55fba7aeb0d7021bf38
Rafael13es <rdehermoso@hotmail.com>
2021-08-30 21:16:32 -0500

    using module pattern

--- assessment_6/assets/js/scripts.js
+++ assessment_6/assets/js/scripts.js
@@ -1,8 +1,25 @@
-document.getElementById("btn").addEventListener("click", onClickChangeText);
+(function () {
+    var Module = {
+        Data: {
+            button: document.getElementById("btn")
+        },
+        Init: function () {
+            this.Events.eventHandler();
+        },
+        Methods: {
+            changeButtonText: function () {
+                var text = document.getElementById("txtbutton").value;
+                if (text.trim().lenght > 0) {
+                    Module.Data.button.innerHTML = text;
+                }
+            }
+        },
+        Events: {
+            eventHandler() {
+                Module.Data.button.addEventListener("click", Module.Methods.changeButtonText)
+            }
+        }
+    }
 
-function onClickChangeText() {
-
-    var text = document.getElementById("txtbutton").value;
-
-    document.getElementById("btn").innerHTML = text;
-}
+    Module.Init();
+})();
